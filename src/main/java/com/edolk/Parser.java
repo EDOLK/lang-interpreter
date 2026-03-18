ce7fcfa7e62bd6b9dc6ba07d83a92a31170e465f
Edward <edwardolkh@gmail.com>
2026-03-18 00:01:34 -0400

    Maven compiler settings

--- src/main/java/com/edolk/Parser.java
+++ src/main/java/com/edolk/Parser.java
@@ -3,9 +3,7 @@ package com.edolk;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Set;
 
 public class Parser {
