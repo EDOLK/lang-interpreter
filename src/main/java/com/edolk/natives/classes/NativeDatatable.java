ef8f5d129cdd86d568083e2f9bde7f34999f365d
Edward <edwardolkh@gmail.com>
2026-04-25 16:23:54 -0400

    feature/row and col method in datatable type

--- src/main/java/com/edolk/natives/classes/NativeDatatable.java
+++ src/main/java/com/edolk/natives/classes/NativeDatatable.java
@@ -33,6 +33,18 @@ public class NativeDatatable extends NativeInstance {
         map.put("entrySize", Callable.create(0, false, (interpreter, args) -> {
             return (double)table.entrySize();
         }));
+        map.put("row", Callable.create(1, false, (interpreter, args) -> {
+            if (args.get(0) instanceof Number number) {
+                return new NativeRow(table.row(number.intValue()));
+            }
+            return null;
+        }));
+        map.put("col", Callable.create(1, false, (interpreter, args) -> {
+            if (args.get(0) instanceof String header) {
+                return new NativeColumn(table.column(header));
+            }
+            return null;
+        }));
         return map;
     }
 
